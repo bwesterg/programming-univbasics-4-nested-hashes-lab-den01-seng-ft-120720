6039656ef6951efa31f82e82654319ca802e895a
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-23 02:18:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,6 +141,7 @@ def adding_to_dennis
     }
 
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-puts adding_to_dennis
+
+programmer_hash[:dennis_ritchie][:languages] = "I'm third!"
 
 end
